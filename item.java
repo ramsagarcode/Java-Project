a4b9182ad55d7f1c1addf77ab9beb90e1f16f652
ramsagarcode <ramsagar78085@gmail.com>
2024-02-28 20:44:18 +0530

    added

---
+++ item.java
@@ -0,0 +1,22 @@
+package org.com;
+
+public class Item {
+	String name;
+	double price;
+
+	public Item(String itemName, double itemPrice) {
+		this.name = itemName;
+		this.price = itemPrice;
+
+	}
+
+	public String getName() {
+		return name;
+
+	}
+
+	public double getPrice() {
+		return price;
+
+	}
+}
